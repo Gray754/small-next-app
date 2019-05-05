505ed41f7bc487fed1ff4360ab2b2724ad9dbdfc
Gray754 <lgc9312@gmail.com>
2019-05-05 16:56:32 -0500

    Added Footer component and included it in Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 import Navbar from './Navbar'
+import Footer from './Footer'
 
 const Layout = (props) => {
     return(
         <div>
             <Navbar/>
             {props.children}
+            <Footer/>
         </div>
     )
 }
