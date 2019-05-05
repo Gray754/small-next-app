5a01662945ee472694d1768fb12f6060aea8500f
Gray754 <lgc9312@gmail.com>
2019-05-05 16:10:29 -0500

    Added a layout for app. Also included two pages.

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () =>{
+    return(
+        <div>
+            <h2>Next Test</h2>
+            <ul>
+                <li><a href='/index'>Home</a></li>
+                <li><a>About</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout'
+
+const Index = () =>{
+    return(
+        <Layout>
+            <div>
+                <h2>Welcome to Next.js!</h2>
+            </div>
+        </Layout>
+    )
+}
+
+export default Index
